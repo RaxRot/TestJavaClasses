a95f956a1ce75ac688749214b1c99920dc7973ef
Vlad Bulahov <dasistperfektosss@gmail.com>
2024-09-17 16:26:25 +0100

    create showMessageFunction

--- src/main.java
+++ src/main.java
@@ -2,4 +2,11 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Test");
     }
+    public void showMessage(String msg){
+        if (msg.isEmpty()){
+            System.out.println("You are empty");
+        }else{
+            System.out.println(msg);
+        }
+    }
 }
